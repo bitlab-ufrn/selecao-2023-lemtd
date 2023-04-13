d8576c96e0fc9e169e1420b04f3092fa817366fe
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-04-13 01:43:10 -0300

    Wip: componente para design do header da página

---
+++ selecao/src/components/Header.js
@@ -0,0 +1,11 @@
+import '../style/Header.css'
+
+const Welcome = () => {
+    return (
+        <div id="header">
+            <h2>Deixe seu comentário abaixo:</h2>
+        </div>
+    )
+}
+
+export default Welcome
