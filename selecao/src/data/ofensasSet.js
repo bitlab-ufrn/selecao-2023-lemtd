f0e105f27c2ebf959cdbbf1880e43066f6b7c6df
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-04-13 01:41:20 -0300

    Conjunto de ofensas utilizado para a verificação de comentários ofensivos

---
+++ selecao/src/data/ofensasSet.js
@@ -0,0 +1,14 @@
+exports.ofensasSet = [
+    "Boquete", "Caralho", "Do caralho", "Foda", "Foda-se",
+    "Foder", "Fuder", "Nem fudendo", "Nem fodendo", "Pau", 
+    "Porra", "Pra caralho", "Puta merda", "Puta que pariu",
+    "Punheta", "Que porra é essa?", "Teu cu", "Cu", "Trepar",
+    "Olho do cu", "Buceta", "Xoxota", "Sacanagem", "Caceta",
+    "Cacete", "Siririca", "Arrombado", "Babaca", "Brocha",
+    "Bicha", "Boiola", "Cracudo", "Filho da puta", "Puta",
+    "Galinha", "Piranha", "Vagabundo", "Viado", "Chupa",
+    "Pica", "Vai tomar no cu", "Corno", "Fudido", "Fodido",
+    "Cachorro", "Cachorra", "Canalha", "Escroto", "Escrota",
+    "Trouxa", "Vaca", "Palhaço", "Merda", "Bosta",
+    "Saco",
+]
