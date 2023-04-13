33a42eaf5c0dd76b54c7d0cd15b6440c455d4cc7
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-04-13 01:37:53 -0300

    Organizando a página em componentes

--- selecao/src/App.js
+++ selecao/src/App.js
@@ -1,35 +1,15 @@
-import {useState} from 'react';
-import './App.css';
+import './style/App.css';
+import Welcome from './components/Welcome';
+import Field from './components/Field';
+import Coment from './components/Coment';
 
 function App() {
-  // Hooks
-  const [valueInput, setValueInput] = useState('');
-
-  const handleInput = (e) => {
-    // Guardando toda modificação feita no input
-    setValueInput(e.target.value);
-
-    console.log(">>> handleInput ", e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    // Evita submissão via GET
-    e.preventDefault();
-
-    // Recebendo os dados via submit
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
-
-    console.log(">>> handleSubmit", data);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="conteudo" placeholder='Insira sua mensagem' onChange={handleInput} value={valueInput}>
-
-      </input>
-      <button type='submit'>Enviar</button>
-    </form>
+    <main className='App'>
+      <Welcome />
+      <Field />
+      <Coment />
+    </main>
   );
 }
 
