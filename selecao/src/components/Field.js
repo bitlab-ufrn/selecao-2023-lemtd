664f59b8af327f3b1c0c28cb2f256758443183b9
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-04-14 04:31:14 -0300

    Reestruturando o component de Field

--- selecao/src/components/Field.js
+++ selecao/src/components/Field.js
@@ -1,48 +1,67 @@
+import { useState } from 'react';
 import '../style/Field.css';
 import Coment from './Coment';
+import Popup from './Popup';
 // import { addComent } from './Coment';
-const data = require('../data/ofensasSet');
+const data = require('../data/offensesSet');
 
-// Hooks
-// const [valueInput, setValueInput] = useState('');
-
-// const handleInput = (e) => {
-//   // Guardando toda modificação feita no input
-//   setValueInput(e.target.value);
-// };
-
-const handleSubmit = (e) => {
-    // Evita submissão via GET
-    e.preventDefault();
-
-    // Recebendo os dados via submit
-    // const formData = new FormData(e.target);
-    // const data = Object.fromEntries(formData);
-};
-
-async function analisarTexto() {
-    var contem = false;
-    var textAreaUpper = document.getElementById('messagem').value.toUpperCase();
-    var textArea = document.getElementById('messagem').value;
-
-    for(let index = 0; index < data['ofensasSet'].length; index++) {
-        var elemento = data['ofensasSet'][index].toUpperCase();
-        if (textAreaUpper.includes(elemento)){
-            console.log('enotrou:::::::: ', elemento);
-            contem = true;
-            break;
+const Field = () => {    
+    // const handleInput = (e) => {
+    //   // Guardando toda modificação feita no input
+    //   setValueInput(e.target.value);
+    // };
+    
+    const handleSubmit = (e) => {
+        // Evita submissão via GET
+        e.preventDefault();
+    
+        // Recebendo os dados via submit
+    };
+    
+    const[content, setContent] = useState(''); //Comentário atual
+    const[containsOffense, setContains] = useState(false); //Bool se contém ofensa
+    
+    async function checkText() {
+        setContains(false);
+        var textAreaToCheckUpper = document.getElementById('messagem').value.toUpperCase();
+        var textArea = document.getElementById('messagem').value;
+    
+        for(let index = 0; index < data['offensesSet'].length; index++) {
+            var element = data['offensesSet'][index].toUpperCase();
+            if (textAreaToCheckUpper.includes(element)){
+                setContains(true);
+                textArea = "Este comentário contém ofensa. Não foi possível adicioná-lo."
+                break;
+            }
         }
+        setContent(textArea);
+        showPopUp();
     }
-}
+    
+    const[popup, setPopup] = useState(false); //Hide/Show popup
+    
+    async function showPopUp() {
+        setPopup(!popup);
+    }
 
-const Field = () => {
-    return (        
-        <form onSubmit={handleSubmit}>
-            <textarea id="messagem" name="conteudo" placeholder='Insira sua mensagem'
-            // onChange={handleInput} value={valueInput}
-            />
-            <button id='enviar' type='submit' onClick={analisarTexto}>Enviar</button>
-        </form>
+    function hidePopup() {
+        setPopup(false);
+    }
+
+    return (   
+        <div className='field'>
+            <form onSubmit={handleSubmit}>
+                <textarea id="messagem" name="conteudo" placeholder='Insira sua mensagem'
+                // onChange={handleInput} value={valueInput}
+                />
+                <button id='enviar' type='submit' onClick={checkText}>Enviar</button>
+            </form>
+            {popup? <Popup
+                        contains={containsOffense}
+                        closeIt={hidePopup}
+                        content={content}/>
+                    : ""}
+        </div>
     )
 }
 
